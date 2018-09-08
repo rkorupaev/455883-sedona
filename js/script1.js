0a3108eca08e0b76794075eab850ad548052b100
rkorupaev <rkorupaev@gmail.com>
2018-09-08 22:33:11 +0300

    button pointer added, blue button fix

--- js/script1.js
+++ js/script1.js
@@ -12,3 +12,27 @@ button.addEventListener("click", function(evt) {
 
 
 });
+
+
+var massive = document.getElementById("adult-amout");
+var value = massive.value;
+var minusButton = document.querySelector(".minus");
+
+minusButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  if (value<= 0) {
+  	value = 0;
+
+  	minusButton.classList.add ("error-class");
+  	// setTimeout(deleteClass() , 10000);
+
+
+  } else {
+  	
+
+  value--;
+  document.getElementById("adult-amout").value = value;
+}
+
+});
